6e1aab29f46f50f4e863d4dbdb0614ee0c4ff097
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2026-01-25 16:52:36 -0700

    feat: Create library

--- src/polars_row_collector/__init__.py
+++ src/polars_row_collector/__init__.py
@@ -1,5 +1,5 @@
 """Facade to collect rows one-by-one into a Polars DataFrame (in the least-bad way)."""
 
-from .polars_row_collector import main
+from .polars_row_collector import PolarsRowCollector
 
-__all__ = ("main",)
+__all__ = ("PolarsRowCollector",)
