f442cf512a1b86cdb4aa7b7854624b6afc15dfd8
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2026-02-01 18:20:49 -0700

    test: combine list-of-dicts vs. PolarsRowCollector perf scripts

--- perf_scripts/perf_test_script.py
+++ perf_scripts/perf_test_script.py
@@ -2,18 +2,23 @@ import gc
 import os
 import time
 from collections.abc import Generator
-from typing import Any, Never
+from typing import Any, Literal, Never, assert_never
 
 import polars as pl
 from tqdm import tqdm
 
 from polars_row_collector.polars_row_collector import PolarsRowCollector
 
-TOTAL_ROWS = int(float(os.getenv("TOTAL_ROWS", "1e6")))
+TOTAL_ROWS = int(float(os.getenv("TOTAL_ROWS", "5e7")))
 CHUNK_SIZE = int(float(os.getenv("CHUNK_SIZE", "25e3")))
 REPORT_EVERY = int(float(os.getenv("REPORT_EVERY", "1e6")))
 DISABLE_GC: bool | int = int(os.getenv("DISABLE_GC", "0"))
 ENABLE_TQDM: bool | int = int(os.getenv("ENABLE_TQDM", "0"))
+
+# Configure comparing against the list-of-dicts style.
+# prc = PolarsRowCollector (this library)
+# dicts = list[dict[str, Any]] (baseline)
+COLLECT_MODE: str = os.getenv("COLLECT_MODE", "prc")  # "dicts" or "prc"
 
 
 def row_generator() -> Generator[dict[str, int | float | str], Any, Never]:
@@ -34,14 +39,21 @@ def main():
     else:
         print("GC left enabled")
 
-    collector = PolarsRowCollector(
-        schema={
-            "a": pl.Int64,
-            "b": pl.Float64,
-            "c": pl.String,
-        },
-        collect_chunk_size=CHUNK_SIZE,
-    )
+    if COLLECT_MODE.lower() == "dicts":
+        list_of_dicts: list[dict[str, Any]] = []
+        collect_mode: Literal["dicts", "prc"] = "dicts"
+    elif COLLECT_MODE.lower() == "prc":
+        collector = PolarsRowCollector(
+            schema={
+                "a": pl.Int64,
+                "b": pl.Float64,
+                "c": pl.String,
+            },
+            collect_chunk_size=CHUNK_SIZE,
+        )
+        collect_mode = "prc"
+    else:
+        raise ValueError(f"Invalid COLLECT_MODE: {COLLECT_MODE}.")
 
     gen = row_generator()
 
@@ -56,7 +68,12 @@ def main():
     last_rows = 0
 
     for i in iterator:
-        collector.add_row(next(gen))
+        if collect_mode == "prc":
+            collector.add_row(next(gen))  # pyright: ignore[reportPossiblyUnboundVariable]
+        elif collect_mode == "dicts":
+            list_of_dicts.append(next(gen))  # pyright: ignore[reportPossiblyUnboundVariable]
+        else:
+            assert_never(collect_mode)
 
         if i % REPORT_EVERY == 0:
             now = time.perf_counter()
@@ -74,7 +91,13 @@ def main():
 
     print("\nFinalizing DataFrame...")
     t0 = time.perf_counter()
-    df = collector.to_df()
+    if collect_mode == "prc":
+        df = collector.to_df()  # pyright: ignore[reportPossiblyUnboundVariable]
+    elif collect_mode == "dicts":
+        df = pl.DataFrame(list_of_dicts)  # pyright: ignore[reportPossiblyUnboundVariable]
+    else:
+        assert_never(collect_mode)
+
     t1 = time.perf_counter()
 
     print(f"Final concat time: {t1 - t0:.2f}s")
